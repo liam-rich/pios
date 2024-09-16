8b64a7e3acbb5562a63010ecf71e6a3b12c8244b
liam-rich <liamcr23@gmail.comexitgit config --global user.email liamcr23@gmail.com>
2024-09-16 15:56:26 -0500

    cleared bss to 0s

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,18 @@
 
-
+char glbl[128];
 
 void kernel_main() {
 
+    extern int __bss_start, __bss_end;
+    char *bssstart, *bssend;
+
+    bssstart = &__bss_start;
+    bssend = &__bss_end;
+   
+
+    while(bssstart < bssend){
+        *bssstart++ = 0;
+}
     while(1){
     }
 }
